f3ee5c0cec6dddca1a3c2970371c0a650aa77228
xianjimli <xianjimli@7eec7cec-e015-11de-ab17-5d3be3536607>
2009-12-17 11:36:28 +0000

    init

---
+++ trunk/cairo-ftk/src/cairo-ftk-surface.c
@@ -0,0 +1,32 @@
+#include <ftk.h>
+#include <pixman.h>
+#include <cairo.h>
+extern cairo_surface_t *
+_cairo_image_surface_create_with_pixman_format (unsigned char		*data,
+						pixman_format_code_t	 pixman_format,
+						int			 width,
+						int			 height,
+						int			 stride);
+
+cairo_surface_t* cairo_ftk_surface_create(FtkWidget* window)
+{
+	int width = 0;
+	int height = 0;
+	FtkBitmap* bitmap = NULL;
+	FtkCanvas* canvas = NULL;
+	FtkColor*  data   = NULL;
+	cairo_surface_t*  surface = NULL;
+	return_val_if_fail(window != NULL, NULL);
+	canvas = ftk_widget_canvas(window);
+	bitmap = ftk_canvas_bitmap(canvas);
+	return_val_if_fail(bitmap != NULL, NULL);
+
+	data = ftk_bitmap_bits(bitmap);
+	width = ftk_bitmap_width(bitmap);
+	height = ftk_bitmap_height(bitmap);
+
+	surface = _cairo_image_surface_create_with_pixman_format(data, PIXMAN_b8g8r8a8, width, height, width * 4);
+
+	return surface;
+}
+
