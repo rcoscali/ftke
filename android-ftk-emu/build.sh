d8d6a9099461260eec020e7c783583605e353cfb
xianjimli <xianjimli@7eec7cec-e015-11de-ab17-5d3be3536607>
2010-07-05 01:26:05 +0000

    auto download openmoko toolchain

--- android-ftk-emu/build.sh
+++ android-ftk-emu/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -d /usr/local/openmoko/arm/ ]
+then
+	echo "No openmoko toolchain installed, install it automatically."
+	wget http://www.limodev.cn/download/openmoko-toolchain.tar.gz
+	tar xf openmoko-toolchain.tar.gz -C /
+fi
+	
+export PATH=/usr/local/openmoko/arm/bin:$PATH
 export ADNROID_SDK_IMAGES_DIR=/work/android/sdk/android-sdk-linux_86/platforms/android-7/images
 
 if [ ! -d $ADNROID_SDK_IMAGES_DIR ]
@@ -8,25 +16,29 @@ then
 	exit 0
 fi
 
+rm -rf ramdisk
+mkdir ramdisk
+
+cd init && make; cd - >/dev/null
+
+echo "Generating ramdisk..."
+
+cd ramdisk && find . | cpio -c -o > ../initrd; cd - >/dev/null
+gzip initrd && mv initrd.gz ramdisk.img
+
+echo "Generating system.img..."
 if [ ! -d rootfs ]
 then
 	tar xf rootfs.tar.gz 
 fi
-
-rm -rf ramdisk
-mkdir ramdisk
-
-cd init && make; cd -
-
-cd ramdisk && find . | cpio -c -o > ../initrd; cd -
-gzip initrd && mv initrd.gz ramdisk.img
-
 ./mkyaffs2image rootfs system.img
 
+echo "Copying files..."
 for f in ramdisk.img system.img
 do
-	mv -fv $f $ADNROID_SDK_IMAGES_DIR
+	echo "  $f --> $ADNROID_SDK_IMAGES_DIR"
+	mv -f $f $ADNROID_SDK_IMAGES_DIR
 done
 
-echo "done"
+echo "Done"
 
